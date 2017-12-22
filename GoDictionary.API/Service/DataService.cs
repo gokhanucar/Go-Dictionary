68a607d7ad1e52fb576ea5c5bb2d981dcf3fada7
Gökhan Uçar <gokhanucar10@gmail.com>
2017-12-22 17:21:56 +0300

    WEB API Service
    Version 1.3.1

---
+++ GoDictionary.API/Service/DataService.cs
@@ -0,0 +1,21 @@
+﻿using GoDictionary.BLL.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoDictionary.API.Service
+{
+    public sealed class DataService
+    {
+        private static readonly EntityService service = new EntityService();
+
+        public static EntityService Service
+        {
+            get
+            {
+                return service;
+            }
+        }
+    }
+}
